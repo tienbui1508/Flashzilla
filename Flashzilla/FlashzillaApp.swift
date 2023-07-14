3c4cfbd39e8a4fdd9af856c19e4044de975ccd9c
tienbui <tienbui1508@gmail.com>
2023-07-14 14:33:20 +0930

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Tien Bui on 14/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
